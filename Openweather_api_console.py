e3030348db04e9925394e27d20c203f2fb17d50e
FireWolfy89 <dmate899@gmail.com>
2025-04-11 13:22:49 +0200

    OpenWeather API console app

---
+++ Openweather_api_console.py
@@ -0,0 +1,44 @@
+import requests
+
+api_key = "2151d7e178017e10631556b4da476ae1"
+
+def input_selector():
+    print("Please provide a city name: ")
+    city = input()
+    return city
+
+def fetch_open_weather(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        return weather_data
+    else:
+        print(f"Could not fetch url: {url}")
+
+def main():
+
+    is_running = True
+
+    while is_running:
+        print("=============================================")
+        print("Welcome to the Python OpenWeather Console application")
+        print("=============================================")
+        print("Press 1. for weather data")
+        print("Press 2. for exit")
+        user_input = input()
+
+        if user_input == "1":
+           city =  input_selector()
+           weather_data = fetch_open_weather(city)
+           print(f"The temperature of {city} is {weather_data["main"]["temp"]} Celsius currently")
+        elif user_input == "2":
+            is_running = False
+            print("Have a nice day!")
+        else:
+            print("Wrong input, please try again")
+
+
+if __name__ == "__main__":
+    main()
